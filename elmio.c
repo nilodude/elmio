c76f1bc19e9f17c7d5f7742531e2af552b3f5b89
erporati <alphone@protonmail.com>
2024-01-31 16:14:13 +0100

    toy

--- elmio.c
+++ elmio.c
@@ -3,19 +3,19 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 
-const uint LED_PIN = 25;
-const uint BUTTON1_PIN = 21;
-const uint BUTTON2_PIN = 22;
-const uint LEDS_PIN = 27;
+const uint LED_PIN = 25; //GPIO25
+const uint BUTTON1_PIN = 20; //GPIO20
+const uint BUTTON2_PIN = 21; //GPIO21
+const uint LEDS_PIN = 19; //GPIO19
 
 int main() {
 
-    // bi_decl(bi_program_description("This is a test binary."));
-    // bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+    bi_decl(bi_program_description("This is a test binary."));
+    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
 
-    // bi_decl(bi_1pin_with_name(BUTTON1_PIN, "button 1"));
-    // bi_decl(bi_1pin_with_name(BUTTON2_PIN, "button 2"));
-    // bi_decl(bi_1pin_with_name(LEDS_PIN, "neopixel strip"));
+    bi_decl(bi_1pin_with_name(BUTTON1_PIN, "button 1"));
+    bi_decl(bi_1pin_with_name(BUTTON2_PIN, "button 2"));
+    bi_decl(bi_1pin_with_name(LEDS_PIN, "neopixel strip"));
 
     stdio_init_all();
 
@@ -27,11 +27,11 @@ int main() {
 
     gpio_init(BUTTON1_PIN);
     gpio_set_dir(BUTTON1_PIN, GPIO_IN);
-    // gpio_pull_up(BUTTON1_PIN);
+    gpio_pull_up(BUTTON1_PIN);
 
     gpio_init(BUTTON2_PIN);
     gpio_set_dir(BUTTON2_PIN, GPIO_IN);
-    // gpio_pull_up(BUTTON2_PIN);
+    gpio_pull_up(BUTTON2_PIN);
 
     while (1) {
         bool button1 = gpio_get(BUTTON1_PIN);
@@ -42,9 +42,7 @@ int main() {
 
         printf("%d %d\n", button1, button2);
 
-        // puts(button1 ? "BUTTON 1 ON" : "BUTTON 1 OFF");
-        // puts(button2 ? "BUTTON 2 ON" : "BUTTON 2 OFF\n\n");
 
-        sleep_ms(1000);
+        sleep_ms(100);
     }
 }
