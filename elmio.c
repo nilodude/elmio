dd03eb3250706bb83d2571e8326048376b27aaec
erporati <alphone@protonmail.com>
2024-02-06 18:33:53 +0100

    encoder OK. still need to handle array of buttons non-explicitly

--- elmio.c
+++ elmio.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 #include "hardware/pio.h"
@@ -25,6 +26,18 @@ struct Button {
 
 struct Button buttons[11] = {};
 
+const uint ENCODER_CLK = 2;
+const uint ENCODER_DT = 3;
+const uint ENCODER_SW = 4;
+
+bool currCLK = 0;
+uint counter = 0;
+bool lastCLK = 0;
+unsigned long lastButtonPress = 0;
+bool pushButton = 0;
+
+bool shouldSwitch = false; //testing led switching when buttons clicked
+
 static inline void put_pixel(uint32_t pixel_grb) {
   pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
 }
@@ -45,10 +58,10 @@ static inline void setup(){
 
     stdio_init_all();
 
+    //LED BUTTONS
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    //should iterate when > 1 "led strip"
     gpio_init(LEDS_PIN);
     gpio_set_dir(LEDS_PIN, GPIO_OUT);
 
@@ -60,17 +73,76 @@ static inline void setup(){
     gpio_set_dir(BUTTON2_PIN, GPIO_IN);
     gpio_pull_up(BUTTON2_PIN);
 
+    //ENCODER
+    gpio_init(ENCODER_CLK);
+    gpio_set_dir(ENCODER_CLK, GPIO_IN);
+    gpio_pull_up(ENCODER_CLK);
+    lastCLK = gpio_get(ENCODER_CLK);
+
+    gpio_init(ENCODER_DT);
+    gpio_set_dir(ENCODER_DT, GPIO_IN);
+    gpio_pull_up(ENCODER_DT);
+
+    gpio_init(ENCODER_SW);
+    gpio_set_dir(ENCODER_SW, GPIO_IN);
+    gpio_pull_up(ENCODER_SW);
+
     for (int i = 0; i < NUMPIXELS; i++) {
       struct Button button = {pins[i],urgb_u32(0x05, 0x10, 0x05),false};
       buttons[i] = button;
     }
 }
 
+static inline void handleEncoder(){
+  currCLK = gpio_get(ENCODER_CLK);
+        
+  if (currCLK != lastCLK && currCLK){
+    if(gpio_get(ENCODER_DT) != currCLK){
+      counter--;
+    }else{
+      counter++;
+    }
+    printf("%d\n",counter);
+  }
+  
+  lastCLK = gpio_get(ENCODER_CLK);
+  
+  pushButton = gpio_get(ENCODER_SW);
+
+  if(!gpio_get(ENCODER_SW)){
+    if(to_ms_since_boot(get_absolute_time()) - lastButtonPress > 50){
+      printf("pulsao\n");
+    }
+    lastButtonPress = to_ms_since_boot(get_absolute_time());
+  }
+}
+
+static inline void handleButtons(){
+  //BUTTON1
+  while(!gpio_get(buttons[0].pin)){
+    buttons[0].pressed = true;
+  }
+  if(buttons[0].pressed){
+    shouldSwitch = !shouldSwitch;
+    printf("%s\n", shouldSwitch ? "1 lima 2 moraito" : "1 moraito 2 lima");
+    buttons[0].pressed = false;
+  }
+
+  //BUTTON2
+  while(!gpio_get(buttons[1].pin)){
+    buttons[1].pressed = true;
+  }
+  if(buttons[1].pressed){
+    shouldSwitch = !shouldSwitch;
+    printf("%s\n", shouldSwitch ? "1 lima 2 moraito" : "1 moraito 2 lima");
+    buttons[1].pressed = false;
+  }
+}
+
 int main() {
 
     setup();
 
-    //each strip can address 8 pixels, maybe for more than 
     PIO pio = pio0;
     int sm = 0;
     uint offset = pio_add_program(pio, &ws2812_program);
@@ -78,58 +150,18 @@ int main() {
 
     ws2812_program_init(pio, sm, offset, LEDS_PIN, 800000, false);
 
-    bool shouldSwitch = false;
-    bool button1pressed = false;
-    bool button2pressed = false;
-
     while (1) {
         gpio_put(LED_PIN, !gpio_get(BUTTON1_PIN) || !gpio_get(BUTTON2_PIN));
         
-        //BUTTON1
-        while(!gpio_get(buttons[0].pin)){
-          buttons[0].pressed = true;
-        }
-        if(buttons[0].pressed){
-          shouldSwitch = !shouldSwitch;
-          buttons[0].pressed = false;
-        }
+        handleEncoder();
 
-        //BUTTON2
-        while(!gpio_get(buttons[1].pin)){
-          buttons[1].pressed = true;
-        }
-        if(buttons[1].pressed){
-          shouldSwitch = !shouldSwitch;
-          buttons[1].pressed = false;
-        }        
+        handleButtons();
         
-        // for (int i = 0; i < NUMPIXELS; i++) {
-        //   struct Button b = buttons[i];
-          
-        //   while(!gpio_get(b.pin)){
-        //     b.pressed = true; 
-        //   }
-        // }
-
-        // for (int i = 0; i < NUMPIXELS; i++) {
-        //   struct Button b = buttons[i];
-
-        //   if(b.pressed){
-        //     shouldSwitch = !shouldSwitch;
-        //     b.pressed = false;
-        //     break;
-        //   }
-        // }
-
-
         for (int i = 0; i < NUMPIXELS; i++) {
           put_pixel(shouldSwitch ? urgb_u32(0x05, 0x10, 0x05) : urgb_u32(0x05, 0, 0x10));
           put_pixel(!shouldSwitch ? urgb_u32(0x05, 0x10, 0x05) : urgb_u32(0x05, 0, 0x10));
         }
 
-        sleep_ms(1);
-        
-        printf("%s\n", shouldSwitch ? "lima" : "moraito");
-        
+        sleep_ms(1);        
     }
 }
